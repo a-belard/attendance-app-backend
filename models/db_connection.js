7488d507358f6a47714dd640a364c529be40dda7
A. Belard <angebelard@Gmail.com>
2022-04-13 12:45:47 +0200

    Update db_connection.js

--- models/db_connection.js
+++ models/db_connection.js
@@ -1,5 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("Mongodb connection established successfully.."))
-.catch(err => console.log(err))
+const connect = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Mongodb connection established successfully.."))
+    .catch((err) => console.log(err));
+
+  require("./student.model");
+  require("./school.model");
+  require("./class.model");
+  require("./teacher.model");
+  require("./class_day.model");
+  require("././attendance.model");
+};
+
+connect();
