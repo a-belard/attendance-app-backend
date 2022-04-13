516d1fe6052c24106c9c8a0edb027cccba9829aa
A. Belard <angebelard@Gmail.com>
2022-04-13 23:34:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const app  = express()
-require("dotenv").config()
-require("./models/db_connection")
+require("dotenv").config();
+require("./models/db_connection");
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-app.use(cors())
-app.use(express.urlencoded({limit:"50mb", extended:"true"}))
-app.use(express.json({limit:"50mb"}))
+const studentRouter = require("./routes/student.route");
 
-app.listen(process.env.PORT || 5000, console.log("Server running on port " + process.env.PORT ))
+app.use(cors());
+app.use(express.urlencoded({ limit: "50mb", extended: "true" }));
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/students", studentRouter);
+
+app.listen(
+  process.env.PORT || 5000,
+  console.log("Server running on port " + process.env.PORT)
+);
