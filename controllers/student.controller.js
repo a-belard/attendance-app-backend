ac88ca4e90bee7b11d0632742f262f7ca1c2518e
A. Belard <angebelard@Gmail.com>
2022-04-13 23:40:19 +0200

    Update student.controller.js

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -7,20 +7,25 @@ studentController.getStudents = async (req, res) => {
   return res.json(students);
 };
 
-studentController.getStudent = (req, res) => {
+studentController.getStudent = async (req, res) => {
   let student = studentRepo.getStudentById(req.params.i);
   if (student) return res.json("No student with that id");
   return res.json(student);
 };
 
-studentController.createStudent = (req,res) => {
-  let exist_student = studentRepo.getStudentByStudentId(req.body.studentId)
-  if (exist_student) return res.status(400).json({success: false, message: "Student already exists"})
-  await studentRepo.createStudent(req.body)
-  .then(student => res.json({success: true, student: student}),
-  err => {
-    console.error(err)
-  res.status(400).json({success: false, err})})
-}
+studentController.createStudent = async (req, res) => {
+  let exist_student = studentRepo.getStudentByStudentId(req.body.studentId);
+  if (exist_student)
+    return res
+      .status(400)
+      .json({ success: false, message: "Student already exists" });
+  await studentRepo.createStudent(req.body).then(
+    (student) => res.json({ success: true, student: student }),
+    (err) => {
+      console.error(err);
+      res.status(400).json({ success: false, err });
+    }
+  );
+};
 
 module.exports = studentController;
