25641e1bfadc027378ba8b92d051ff220bbcb8cb
A. Belard <angebelard@Gmail.com>
2022-04-13 23:40:17 +0200

    Update class.controller.js

--- controllers/class.controller.js
+++ controllers/class.controller.js
@@ -7,20 +7,20 @@ classController.getClasses = async (req, res) => {
   return res.json(classes);
 };
 
-classController.getClass = (req, res) => {
+classController.getClass = async (req, res) => {
   let _class = classRepo.getClassById(req.params.i);
   if (!_class) return res.json("No class with that id");
   return res.json(_class);
 };
 
-classController.createClass = (req,res) => {
-  let exist_class = classRepo.getClassById(req.body.classId)
-  if (exist_class) return res.status(400).json({success: false, message: "class already exists"})
-  await classRepo.createclass(req.body)
-  .then(newClass => res.json({success: true, class: newClass}),
-  err => {
-    console.error(err)
-  res.status(400).json({success: false, err})})
-}
+classController.createClass = async (req, res) => {
+  await classRepo.createclass(req.body).then(
+    (newClass) => res.json({ success: true, class: newClass }),
+    (err) => {
+      console.error(err);
+      res.status(400).json({ success: false, err });
+    }
+  );
+};
 
 module.exports = classController;
