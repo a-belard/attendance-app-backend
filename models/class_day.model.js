295766c2d0fb6017d77bd408bbc655c251783a85
A. Belard <angebelard@Gmail.com>
2022-04-13 13:04:40 +0200

    Update class_day.model.js

--- models/class_day.model.js
+++ models/class_day.model.js
@@ -5,6 +5,7 @@ const classday_schema = new mongoose.Schema(
     month: { type: String },
     date: { type: Number },
     year: { type: Number },
+    status: { type: Boolean },
   },
   {
     timestamps: true,
