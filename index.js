e19f025ed2a8c6c12b80b9f956c0ca7e090fad91
A. Belard <angebelard@Gmail.com>
2022-04-13 23:40:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const cors = require("cors");
 const app = express();
 
 const studentRouter = require("./routes/student.route");
+const classRouter = require("./routes/class.route");
 
 app.use(cors());
 app.use(express.urlencoded({ limit: "50mb", extended: "true" }));
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/students", studentRouter);
+app.use("/classes", classRouter);
 
 app.listen(
   process.env.PORT || 5000,
