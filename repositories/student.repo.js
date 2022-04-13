086c6340625710d51d295e469a5fe347330fc434
A. Belard <angebelard@Gmail.com>
2022-04-13 13:04:51 +0200

    Update student.repo.js

--- repositories/student.repo.js
+++ repositories/student.repo.js
@@ -4,10 +4,23 @@ let getStudents = async () => {
   return await Student.find();
 };
 
-let getStudentById = async (id) => {
-  return await Student.findById(id);
+let getStudentById = async (_id) => {
+  return await Student.findById(_id);
+};
+
+let getStudentByStudentId = async (studentId) => {
+  return await Student.findOne(studentId);
 };
 
 let getStudentsByClass = async (classId) => {
   return await Student.find({ classId });
+};
+
+let createStudent = async (student) => {
+  let newStudent = new Student(student);
+  return await newStudent.save();
+};
+
+let updateStudent = async (_id, student) => {
+  return await Student.findByIdAndUpdate();
 };
