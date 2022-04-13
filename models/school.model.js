c53c2a402c8868efd56fc8d3124b4566f3a761ca
A. Belard <angebelard@Gmail.com>
2022-04-13 13:04:42 +0200

    Update school.model.js

--- models/school.model.js
+++ models/school.model.js
@@ -5,6 +5,7 @@ const schoolSchema = new mongoose.Schema(
     name: { type: String },
     location: { type: String },
     head_master: { type: String },
+    status: { type: Boolean },
   },
   {
     timestamps: true,
